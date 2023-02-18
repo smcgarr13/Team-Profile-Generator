c000c9a0bb1cb89622e7dff8cecae151ea12e957
Sparkles <smcgarr13@yahoo.com>
2023-02-18 14:46:01 -0600

    updated-class-files

--- index.js
+++ index.js
@@ -1,108 +1,120 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const jest = require('jest');
+const express = require('express');
+const app = express();
+const templateHelperCode = require('template-helper-code');
 
-// TODO: Create classes for: Employee, Manager, Engineer, and Intern
+// // TODO: Create classes for: Employee, Manager, Engineer, and Intern
 
 
-// TODO: The first class is an Employee parent class with the following properties and methods:
+// // TODO: The first class is an Employee parent class with the following properties and methods:
 
-class Employee {
-    constructor(name, id, email) {
-      // validation
-      if (!name || typeof name !== 'string') {
-        throw new Error('Employee name cannot be an empty string')
-      }
-      if (!id || typeof id !== 'string') {
-        throw new Error('Employee id cannot be an empty string')
-      }
-      if (!email || typeof email !== 'string') {
-        throw new Error('Employee email cannot be an empty string')
-      }
+// class Employee {
+//     constructor(name, id, email) {
+//       // validation
+//       if (!name || typeof name !== 'string') {
+//         throw new Error('Employee name cannot be an empty string')
+//       }
+//       if (!id || typeof id !== 'string') {
+//         throw new Error('Employee id cannot be an empty string')
+//       }
+//       if (!email || typeof email !== 'string') {
+//         throw new Error('Employee email cannot be an empty string')
+//       }
 
-      this.name = name;
-      this.id = id;
-      this.email = email;
-    //   this.createdOn = createdOn;
-    //   this.comments = [];
-    }
+//       this.name = name;
+//       this.id = id;
+//       this.email = email;
+//     //   this.createdOn = createdOn;
+//     //   this.comments = [];
+//     }
 
-    // method to print the meta data
-    printMetaData() {
-        console.log('Name: ${this.name}\n\nid: ${this.id}\n\nemail: ${this.email}')
+//     // method to print the meta data
+//     printMetaData() {
+//         console.log('Name: ${this.name}\n\nid: ${this.id}\n\nemail: ${this.email}')
+//     };
+
+//     getName() {
+//         return this.name;
+//     }
+
+//     getId() {
+//         return this.id;
+//     }
+
+//     getEmail() {
+//         return this.email;
+//     }
+// // Returns 'Employee'
+//     getRole() {
+//     return 'Employee';
+//     }
+// }
+
+
+// // TODO: The other three classes will extend Employee.  In addition to Employee's properties and methods, Manager will also have "officeNumber"
+
+// class Manager extends Employee {
+//     constructor(name, id, email, officeNumber) {
+//         super(name, id, email)
+//         if (!officeNumber || typeof officeNumber !== 'string') {
+//             throw new Error('Manager office number cannot be an empty string')
+//           }
+//         this.officeNumber = officeNumber;
+//     }
+//     // Overridden to return 'Manager'
+//     getRole() {
+//         return 'Manager';
+//     }
+// }
+
+// // TODO: In addition to Employee's properties and methods, Engineer will also have "github" (GitHub username)
+
+// class Engineer extends Employee {
+//     constructor(name, id, email, github) {
+//         super(name, id, email)
+//         if (!github || typeof github !== 'string') {
+//             throw new Error('Engineer github cannot be an empty string')
+//           }
+//         this.github = github;
+//     }
+//     getGithub() {
+//         return this.github
+//     }
+// // Overridden to return 'Engineer'
+//     getRole() {
+//         return 'Engineer';
+//     }
+// }
+
+
+// // TODO: In addition to Employee's properties and methods, Intern will also have "school"
+
+// class Intern extends Employee {
+//     constructor(name, id, email, school) {
+//         super(name, id, email)
+//         if (!school || typeof school !== 'string') {
+//             throw new Error('Intern school cannot be an empty string')
+//           }
+//         this.school = school;
+//     }
+//     getSchool() {
+//         return this.school
+//     }
+// // Overridden to return 'Intern'
+//     getRole() {
+//         return 'Intern';
+//     }
+// }
+// // run
+// // TODO: Finally, although it’s not a requirement, you should consider adding validation to ensure that user input provided is in the proper expected format.
+
+// dynamically create HTML file
+app.get('/', (req, res) => {
+    const data = {
+        title: '',
+        message: ''
     };
-
-    getName() {
-        return this.name;
-    }
-
-    getId() {
-        return this.id;
-    }
-
-    getEmail() {
-        return this.email;
-    }
-// Returns 'Employee'
-    getRole() {
-    return 'Employee';
-    }
-}
-
-
-// TODO: The other three classes will extend Employee.  In addition to Employee's properties and methods, Manager will also have "officeNumber"
-
-class Manager extends Employee {
-    constructor(name, id, email, officeNumber) {
-        super(name, id, email)
-        if (!officeNumber || typeof officeNumber !== 'string') {
-            throw new Error('Manager office number cannot be an empty string')
-          }
-        this.officeNumber = officeNumber;
-    }
-    // Overridden to return 'Manager'
-    getRole() {
-        return 'Manager';
-    }
-}
-
-// TODO: In addition to Employee's properties and methods, Engineer will also have "github" (GitHub username)
-
-class Engineer extends Employee {
-    constructor(name, id, email, github) {
-        super(name, id, email)
-        if (!github || typeof github !== 'string') {
-            throw new Error('Engineer github cannot be an empty string')
-          }
-        this.github = github;
-    }
-    getGithub() {
-        return this.github
-    }
-// Overridden to return 'Engineer'
-    getRole() {
-        return 'Engineer';
-    }
-}
-
-
-// TODO: In addition to Employee's properties and methods, Intern will also have "school"
-
-class Intern extends Employee {
-    constructor(name, id, email, school) {
-        super(name, id, email)
-        if (!school || typeof school !== 'string') {
-            throw new Error('Intern school cannot be an empty string')
-          }
-        this.school = school;
-    }
-    getSchool() {
-        return this.school
-    }
-// Overridden to return 'Intern'
-    getRole() {
-        return 'Intern';
-    }
-}
-// run
-// TODO: Finally, although it’s not a requirement, you should consider adding validation to ensure that user input provided is in the proper expected format.
+    res.render('template-file' {data, templateHelperCode});
+});
--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,43 +1,47 @@
 // const Employee = require('./Employee')
-const Employee = require('../lib/Employee');
+const Employee = require('../lib/Employee.js');
 
 test('Can set name via constructor argument', () => {
     const name = 'Mugi';
-    const employee = new Employee(name);
+    const id = 813;
+    const email = 'mugi@magical.com';
+    const employee = new Employee(name, id, email);
     expect(employee.name).toBe(name);
 });
 
 test('Can set id via constructor argument', () => {
     const id = 813;
-    const employee = new Employee('Foo', id);
+    const employee = new Employee('Foo', id, 'mugi@magical.com');
     expect(employee.id).toBe(id);
 });
 
 test('Can set email via constructor argument', () => {
     const email = 'mugi@magical.com';
-    const employee = new Employee('Foo', 1, email);
+    const employee = new Employee('Foo', 813, email);
     expect(employee.email).toBe(email);
 });
 
 test('getName() should return name', () => {
     const name = 'Mugi';
-    const employee = new Employee(name);
+    const id = 813;
+    const employee = new Employee(name, id, 'mugi@magical.com');
     expect(employee.getName()).toBe(name);
 });
 
 test('getId() should return id', () => {
-    const name = '813';
-    const employee = new Employee('Foo', id);
+    const id = 813;
+    const email = 'mugi@magical.com';
+    const employee = new Employee('Foo', id, email);
     expect(employee.getId()).toBe(id);
 });
 
 test('getEmail() should return email', () => {
     const email = 'mugi@magical.com';
-    const employee = new Employee('Foo', 1, email);
+    const employee = new Employee('Foo', 813, email);
     expect(employee.getEmail()).toBe(email);
 });
 
 test('getRole() should return "Employee"', () => {
-    const employee = new Employee('Foo', 1, 'mugi@magical.com');
+    const employee = new Employee('Foo', 813, 'mugi@magical.com');
     expect(employee.getRole()).toBe('Employee');
 });
