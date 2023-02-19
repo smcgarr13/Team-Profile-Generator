201902ccb2ba25d9232ff74475318be38960c6fd
Sparkles <smcgarr13@yahoo.com>
2023-02-18 23:19:19 -0600

    add-style

--- src/template-helper-code.js
+++ src/template-helper-code.js
@@ -48,8 +48,10 @@ const mainBody = (cards) => {
             <meta charset="utf-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <title>My Team</title>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-+iLwuix06anLfy4M4jreP8cQZd7Btmhp+cm1S/AiBFlo9bs1Y7zTtLQD0nRjJ8sCt7NHu/a/WvF3/nlzNGX9og==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-            <link rel="stylesheet" href="./style.css">
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Pacifico&display=swap" rel="stylesheet">
+            <link rel="stylesheet" href="./dist/style.css" />
           </head>
           <body>
             <header>
